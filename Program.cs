fe14ec5a0c187128031ea8248102474fdb5688ee
KevinSchinaider <KevinSchinaider05@outlook.com>
2022-09-30 21:53:27 -0300

    Solução Exercício

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int n1, n2, resto;
+
+Console.WriteLine("----------------");
+Console.WriteLine("----Múltiplo----");
+Console.WriteLine("----------------");
+
+Console.WriteLine("Digite o primeiro número: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+resto = n1 % n2;
+
+bool ehMultiplo = resto == 0;
+
+if (resto == 0)
+{
+    Console.WriteLine($"{n1} é múltiplo de {n2}");
+}
+else
+{
+    Console.WriteLine($"{n1} não é múltiplo de {n2}");
+
+}
